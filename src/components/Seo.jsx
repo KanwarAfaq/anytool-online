9eccbadff95614c13ff9385b3e52e9106c05f2ef
Kanwar Muhammad Afaq <kmafaq786@gmail.com>
2026-09-20 15:09:07 +0800

    Use production www canonical in src/components/Seo.jsx

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
-const SITE='https://anytool.online'
+const SITE='https://www.anytool.online'
 const locales=[
   ['en',''],
   ['zh-TW','/zh-tw'],
